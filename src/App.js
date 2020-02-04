818fbc3911719bc0ec423b10110d2bfd4d09ad21
Jason Neale <jason@novus.one>
2020-02-04 18:23:31 -0500

    Updated Starting Structure

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import React, {useState, useEffect} from "react"
 import axios from 'axios'
 import "./App.css";
 
-import Header from "./header/Header"
-import Card from "./card/Card"
-import Footer from "./footer/Footer"
+import Header from "./components/header/Header"
+import Card from "./components/card/Card"
+import Footer from "./components/footer/Footer"
 
 function App() {
     const apiKey = "CecIldgQmXLCCTnr02fOh2srCshbmyIYKYPHc7EA"
     const [apiData, setApiData] = useState([])
 
     useEffect(() => {
-        axios.get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`).then(res => {setApiData(res.data)}).catch(err => {console.log('axios error: ', err.data)})
+        axios
+            .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+            .then(res => { setApiData(res.data) })
+            .catch(err => { console.log('axios error: ', err.data) })
     }, [])
 
     return (
         <div className="App">
             <Header />
+            <Card />
+            <Footer />
         </div>
     );
 }
