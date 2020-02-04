6d610035671430d1a94dc7bda91c29844e56d2ff
Jason Neale <jason@novus.one>
2020-02-04 17:19:23 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react"
+import axios from 'axios'
 import "./App.css";
 
+import Header from "./header/Header"
+import Card from "./card/Card"
+import Footer from "./footer/Footer"
+
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+    const apiKey = "CecIldgQmXLCCTnr02fOh2srCshbmyIYKYPHc7EA"
+    const [apiData, setApiData] = useState([])
+
+    useEffect(() => {
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`).then(res => {setApiData(res.data)}).catch(err => {console.log('axios error: ', err.data)})
+    }, [])
+
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    );
 }
 
 export default App;
